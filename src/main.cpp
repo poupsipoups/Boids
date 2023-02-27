253812ceecde946220c6afa6bc709b16b713b9a2
Pauline Gobé <pauline.gobe16@gmail.com>
2023-02-27 16:18:08 +0100

    rectangles away

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <cstdlib>
+#include "glm/fwd.hpp"
 #include "p6/p6.h"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
@@ -15,16 +17,62 @@ int main(int argc, char* argv[])
     }
 
     // Actual app
-    auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
+    auto ctx = p6::Context{{.title = "ProgS4"}};
     ctx.maximize_window();
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    std::vector<glm::vec2> rectangles(100);
+    std::vector<int>       directions(100);
+
+    // initialisation des positions de rectangle
+    for (auto& rectangle : rectangles)
+    {
+        x = p6::random::number(-2, 2);
+        y = p6::random::number(-1, 1);
+
+        rectangle = glm::vec2(x, y);
+    }
+
+    for (auto& direction : directions)
+    {
+        direction = p6::random::number(0, 3);
+    }
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        // Clear the background with a fading effect
+        ctx.use_stroke = false;
+        ctx.fill       = {0.2f, 0.1f, 0.3f, 0.1f};
+        ctx.rectangle(p6::FullScreen{});
+
+        ctx.fill = {1.f, 0.7f, 0.2f};
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (directions[i] == 0)
+            {
+                rectangles[i].y += 0.01;
+            }
+            else if (directions[i] == 1)
+            {
+                rectangles[i].x += 0.01;
+            }
+            else if (directions[i] == 2)
+            {
+                rectangles[i].y -= 0.01;
+            }
+            else
+            {
+                rectangles[i].x -= 0.01;
+            }
+
+            ctx.square(
+                p6::Center{rectangles[i].x, rectangles[i].y},
+                p6::Radius{0.1f}
+            );
+        }
     };
 
     // Should be done last. It starts the infinite loop.
